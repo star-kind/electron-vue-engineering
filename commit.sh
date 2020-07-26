43bdec5ec2a77d81d6f40dbbf290bb5906818096
AllStarGH <duernna@163.com>
2020-07-26 13:01:04 +0800

    bilibili缓存视频处理模块再优化,修正了对国漫缓存的支持.

--- commit.sh
+++ commit.sh
@@ -8,7 +8,7 @@ git status
 
 git add .  
 
-git commit -m "user模块:密码更改模块已完成;"
+git commit -m "bilibili缓存视频处理模块再优化,修正了对国漫缓存的支持."
 
 # 提示：更新被拒绝，因为您当前分支的最新提交落后于其对应的远程分支。
 # 提示：再次推送前，先与远程变更合并（如 'git pull ...'）。详见
--- moveAndRename-GUI/src/renderer/utils/bilibiliUtils/getVideoNewName.js
+++ moveAndRename-GUI/src/renderer/utils/bilibiliUtils/getVideoNewName.js
@@ -6,16 +6,35 @@
  */
 const getBiliVideoNewName = (jsonObject) => {
     var strArr = []
-    for (let index in jsonObject) {
-        let str = ''
-        str += jsonObject[index].avid;
-        str += '-'
-        str += jsonObject[index].title.replace(/【/g, '').replace(/】/g, '').replace(/《/g, '').replace(/》/g, '');
-        str += '-'
-        str += jsonObject[index].page_data.page;
-        str += '.mp4';
-     
-        strArr.push(str);
+    try {
+        for (let index in jsonObject) {
+            let str = ''
+            str += jsonObject[index].avid;
+            str += '-'
+            str += jsonObject[index].title.replace(/【/g, '').replace(/】/g, '').replace(/《/g, '').replace(/》/g, '').replace(/\//g, '');
+            str += '-'
+            str += jsonObject[index].page_data.page;
+            str += '.mp4';
+
+            strArr.push(str);
+        }
+    } catch (err) {
+        console.log('国产动画缓存...')
+        console.log(err);
+        for (let n in jsonObject) {
+            let str = '';
+            str += jsonObject[n].ep.av_id;
+            str += '-';
+            str += jsonObject[n].title;
+
+            str += '-'
+            str += jsonObject[n].ep.index_title.replace(/【/g, '').replace(/】/g, '').replace(/《/g, '').replace(/》/g, '');
+            str += '-'
+            str += jsonObject[n].ep.page;
+            str += '.mp4';
+
+            strArr.push(str);
+        }
     }
     console.table(strArr)
     return strArr;
